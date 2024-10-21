e4d0c32163c732428af8fa9ef02fa491313b3863
PabloVasquez <vascor.pablo@gmail.com>
2024-10-20 21:13:16 -0300

    top-bar & side-bar V1

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Item } from '../models/top-bar/item';
+import { SideBarComponent } from '../side-bar/side-bar.component';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css'
 })
 export class TopBarComponent {
+  items: Item[] = []
+  
+
 
 }
